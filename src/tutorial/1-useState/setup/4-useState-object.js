f3e3ecacb12fd0bc0e404b84747c31ee58422d92
Nyuykisamo Lamfih <lamfihbilly@hotmail.com>
2021-07-30 06:49:12 +0100

    counter-example

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [object, setObject] = useState({
+    age: 21,
+    name: "Billy-Brown",
+    message: "I am gonna make this money",
+  });
+  const editObject = () => {
+    setObject({ ...object, name: "shmurda" });
+    // setObject({ ...object, age: "27" });
+    // setObject({ ...object, message: "We gone pull on em" });
+  };
+  return (
+    <>
+      <h3>{object.age}</h3>
+      <h3>{object.name}</h3>
+      <h4>{object.message}</h4>
+      <button className="btn" onClick={editObject}>
+        Editing
+      </button>
+    </>
+  );
 };
 
 export default UseStateObject;
